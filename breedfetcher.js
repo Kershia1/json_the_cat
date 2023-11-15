507176adad5dd0fc4c01bec1cc867645a8bec49e
Kershia1 <insatiable.meerkat@gmail.com>
2023-11-15 06:39:51 +0000

    parsed JSON file and retrieved 1st entry in data array.

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,4 +1,18 @@
 const request = require('request');
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
+  const data = JSON.parse(body);
+  let results = data[0];
+  //console.log(results);//entire data array returned 
+  //console.log('weight', results.weight);// works retrieve the parsed data array from the object to manipulate and acces the key.values.
+  // console.log(data); //undefined
+  // console.log(typeof data); //object
+  // console.error('error; ', error);
+  // console.log('statusCode: ', response && response.statusCode);
+  // console.log('body : ', body);
+  //console.log(typeof body); // returns vagrant [json_the_cat]> node breedfetcher.js
+  //string
+
+});
 
 /*
 Implementing in Node
@@ -32,6 +46,21 @@ Access the first entry in the data array and print out the description for the u
 //specfiy the endpoint for the /get/breeds/search request 
 //for siberian cat https://api.thecatapi.com/v1/breeds/search?q=sib
 
+// 1)
 //fetch breed information from the catsAPI
 
+
+
+//2)
 //create the callback to implement this. 
+
+
+//to check what data is being returned in the body of the request.
+
+// the stringifyed return then is converted to a JSON File 
+// const data = JSON.parse(body);
+// console.log(data);
+// console.log(typeof data);
+
+//3
+//Access the first entry in the data array and print out the description for the user.
